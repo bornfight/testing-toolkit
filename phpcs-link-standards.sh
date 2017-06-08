a06dbca4480fe1e4c55ccc29afc3407094134b66
Marko Kruljac <marko.kruljac@degordian.com>
2017-06-08 11:30:59 +0200

    updated

--- phpcs-link-standards.sh
+++ phpcs-link-standards.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
-ln -s vendor/degordian/php-coding-standards/Degordian vendor/squizlabs/php_codesniffer/src/Standards/
-ln -s vendor/yiisoft/yii2-coding-standards/Yii2 vendor/squizlabs/php_codesniffer/src/Standards/
+cd "${0%/*}" #set working directory to testing-toolkit
+ln -s $(pwd)/vendor/degordian/php-coding-standards/Degordian vendor/squizlabs/php_codesniffer/src/Standards/
+ln -s $(pwd)/vendor/yiisoft/yii2-coding-standards/Yii2 vendor/squizlabs/php_codesniffer/src/Standards/
