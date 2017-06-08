fde3b1725742f90470f95537a9e98784cf182f86
Marko Kruljac <marko.kruljac@degordian.com>
2017-06-08 11:21:32 +0200

    updated gitignore

---
+++ phpcs-link-standards.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+ln -s vendor/degordian/php-coding-standards/Degordian vendor/squizlabs/php_codesniffer/src/Standards/
+ln -s vendor/yiisoft/yii2-coding-standards/Yii2 vendor/squizlabs/php_codesniffer/src/Standards/
