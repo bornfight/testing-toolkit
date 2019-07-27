a7139137a5540423e1cd8170d8975ffa09c615dd
Marko Kruljac <marko.kruljac@degordian.com>
2019-07-27 10:29:50 +0200

    Refactor for new major release

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# Degordian testing toolkit
-A collection of various testing and static-analysis tools that are used internally.
+# Bornfight testing toolkit
+A collection of various testing and static-analysis tools that are used in our projects.
--- phpcs-link-standards.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-cd "${0%/*}" #set working directory to testing-toolkit
-ln -s $(pwd)/vendor/degordian/php-coding-standards/Degordian vendor/squizlabs/php_codesniffer/src/Standards/
-ln -s $(pwd)/vendor/yiisoft/yii2-coding-standards/Yii2 vendor/squizlabs/php_codesniffer/src/Standards/
